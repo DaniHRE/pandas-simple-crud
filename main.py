dbf986c3155a5cb39f362c2bff6479d344ab1191
danihre <henriqueevaldo@outlook.com>
2022-06-09 10:35:35 -0300

    Add update and delete

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ pd.set_option(
     'expand_frame_repr', False)
 
 # Pandas base config
-df = pd.read_csv('Estabelecimento.csv', encoding='ISO-8859-1', sep=',').fillna("")
+fileDestination = 'Estabelecimento.csv'
+df = pd.read_csv(fileDestination, encoding='ISO-8859-1', sep=',').fillna("")
 df.style.hide(axis='index')
 data = pd.DataFrame
 df = df.convert_dtypes()
@@ -51,6 +52,14 @@ phoneRegex = re.compile(r'(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})')
 # 3: 000000000-00
 # 4: 00000000/0000-00
 cnpjRegex = re.compile(r'([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{14})')
+
+
+def saveChanges(dataFrame):
+    opUser = input('Are you sure want to overwrite table? [Y/N]: ').upper()
+    if opUser == 'Y':
+        dataFrame.to_csv(fileDestination, sep=',', encoding='ISO-8859-1', index=False)
+    else:
+        input('Operation Canceled. \n')
 
 
 def clearScreen():
@@ -110,7 +119,7 @@ def addValue():
     baseDf = pd.DataFrame(columns=column_names)
 
     print("\n============================"
-          "\n|   Required fields:   |"
+          "\n|     Required fields:     |"
           "\n============================"
           "\n|    Nome  |  CEP  | Email |"
           "\n|  Tel. Principal  | CNPJ  |"
@@ -162,27 +171,11 @@ def addValue():
             print(tabulate(data(updateDf[headersList].tail()), headers='keys', tablefmt='pretty'))
 
             # Atualiza o DataFrame com as informações colocadas pelo usuário.
-            updateDf.to_csv('Estabelecimento.csv', sep=';', encoding='utf-8', index=False)
+            updateDf.to_csv('Estabelecimento.csv', sep=',', encoding='ISO-8859-1', index=False)
             break
 
 
-def updateValue():
-    opUser, opLimit = viewColumn()
-
-    inputValue = input('What value do you want to change? [STRING]: ')
-
-    #  CRIA UM NOVO DATAFRAME COM TODAS AS LINHAS DA COLUNA ESPECIFICADA PELO USUÁRIO
-    ndf = pd.DataFrame(df[df.iloc[:, int(opUser) - 1].name][:opLimit])
-
-    #  DA REPLACE EM TODOS OS VALORES QUE O USUÁRIO QUISER (VALORES IGUAIS)
-    ndf.replace([inputValue], 'TESTE', inplace=True)
-
-    #  PRINTA O DATAFRAME COM AS NOVAS ALTERAÇÕES
-    print(tabulate(data(ndf), headers='keys', tablefmt='pretty'))
-
-
 def deleteValue():
-    newDf = df
     opUser, opLimit = viewColumn()
 
     while True:
@@ -190,13 +183,46 @@ def deleteValue():
 
         hasContinue = input("want to remove more lines? [Y/N]:").upper()
         if hasContinue == "Y":
-            newDf = newDf.drop(deleteChoice)
-            print(tabulate(data(newDf[df.iloc[:, int(opUser) - 1].name][:opLimit]), headers='keys', tablefmt='pretty'))
+            df.drop(deleteChoice, axis=0, inplace=True)
+            print(tabulate(data(df[df.iloc[:, int(opUser) - 1].name][:opLimit]), headers='keys', tablefmt='pretty'))
             continue
         else:
-            newDf = newDf.drop(deleteChoice)
-            print('Those are like.')
-            print(tabulate(data(newDf[df.iloc[:, int(opUser) - 1].name][:opLimit]), tablefmt='pretty'))
+            df.drop(deleteChoice, axis=0, inplace=True)
+
+            # PRINTS TABLE WITH MODIFICATIONS
+            print('\nTHOOSE ARE LIKE:')
+            print(tabulate(data(df[df.iloc[:, int(opUser) - 1].name][:opLimit]), headers='keys', tablefmt='pretty'))
+
+            # PUT IN ORIGINAL DESTINATION FILE
+            df.to_csv('Estabelecimento.csv', sep=',', encoding='ISO-8859-1', index=False)
+            break
+
+
+def updateValue():
+    opUser, opLimit = viewColumn()
+
+    #  CREATE A NEW DATAFRAME WITH ALL USER-SPECIFIED COLUMN ROWS
+    ndf = pd.DataFrame(df[df.iloc[:, int(opUser) - 1].name][:opLimit])
+
+    while True:
+        # ANSWER TO INDEX and INPUT TO REFERENT INDEX
+        updateChoice = int(input('Enter the line you want to update: [INDEX]: '))
+        inputValue = input('What value do you want to change? [STRING]: ')
+
+        #  REPLACE IN ALL VALUES THAT THE USER WANTS (EQUAL VALUES)
+        ndf.at[updateChoice, df.iloc[:, int(opUser) - 1].name] = inputValue
+
+        #  PRINTS DATAFRAME WITH NEW MODIFICATIONS
+        print(tabulate(data(ndf), headers='keys', tablefmt='pretty'))
+
+        # IF USER WISH TO CONTINUE
+        hasContinue = input('Want to Update New Lines? [Y/N]: ').upper()
+        if hasContinue == 'Y':
+            continue
+        else:
+            df.at[updateChoice, df.iloc[:, int(opUser) - 1].name] = inputValue
+            saveChanges(df)
+            break
 
 
 if __name__ == '__main__':
@@ -205,8 +231,8 @@ if __name__ == '__main__':
     # viewHeaders()
     # searchInTable()
     # addValue()
-    # updateValue()
     # deleteValue()
+    # updateValue()
 
     # FOR NEXT UPDATE PASS TO CLASS OBJECTS AND CREATE A MAIN MENU TO SELECT OPTIONS
     pass
